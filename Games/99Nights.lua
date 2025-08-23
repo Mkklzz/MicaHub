43fe12b0aa8960a7204af256413780093c24e774
Mkklzz <micakklzz@gmail.com>
2025-08-23 19:39:39 -0400

    Update 99Nights.lua

--- Games/99Nights.lua
+++ Games/99Nights.lua
@@ -9,12 +9,6 @@ local RemoveCooldownsExecutionStatusVariable = false
 local ToraLibraryInterfaceReference = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mkklzz/MicaHub/Home/ToraLibrarySource.lua"))()
 local MainWindowTabInterfaceContainer = ToraLibraryInterfaceReference:CreateWindow("99 Nights")
 
-local function CheckAllFunctionsActivated()
-    if UnlockFramesPerSecondExecutionStatusVariable and WalkSpeedModificationExecutionStatusVariable and RemoveCooldownsExecutionStatusVariable then
-        ToraLibraryInterfaceReference:Close()
-    end
-end
-
 MainWindowTabInterfaceContainer:AddButton({
     text = "Remove Cooldowns",
     flag = "button",
@@ -239,3 +233,9 @@ MainWindowTabInterfaceContainer:AddLabel({
 })
 
 ToraLibraryInterfaceReference:Init()
+
+local function CheckAllFunctionsActivated()
+    if UnlockFramesPerSecondExecutionStatusVariable and WalkSpeedModificationExecutionStatusVariable and RemoveCooldownsExecutionStatusVariable then
+        ToraLibraryInterfaceReference:Close()
+    end
+end
